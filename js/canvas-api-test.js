660c31bfad0737504b50ef318709f52759132a8e
Francisco Jose Morales <morales0@umd.edu>
2019-10-08 10:08:40 -0400

    add files

---
+++ js/canvas-api-test.js
@@ -0,0 +1,21 @@
+var accessToken =
+   "113~yx5LBQDrCgBkNQosFXdAIn8K4rCCXZerEeUvKsET1ZQtBSPIEdc2GMCmm9E2y45w";
+
+/*
+$.ajax({
+   type: "GET",
+   url: "https://umd.beta.instructure.com/api/v1/courses",
+   dataType: "json",
+   headers: {
+      Authorization: "Bearer " + accessToken,
+      "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
+      "Content-Type": "application/json",
+      Accept: "application/json"
+   },
+   success: function(data) {
+      data.forEach(element => {
+         console.log(element.name);
+      });
+   }
+});
+*/
